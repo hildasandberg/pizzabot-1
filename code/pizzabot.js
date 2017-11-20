87b502db76dd01176cd5dd114a1e1c02a2d24b95
Hilda <hilda.sandberg@gmail.com>
2017-11-20 13:56:20 +0100

    Strecth goals completed

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -38,9 +38,9 @@ const orderTotal=totalCost(orderQuantity)
 const cookingTime= (quant) => {
   if (quant < 3) {
     return 10
-  } else if (2 < quant && quant < 6) {
+  } else if (quant >= 3 && quant <= 5) {
     return 15
-  } else if (6 < quant) {
+  } else if (quant >=6) {
     return 20
   }
 }
@@ -49,6 +49,23 @@ const Time=cookingTime(orderQuantity)
 
 alert("Great, I'll get started on your " + orderName + " right away, it will cost " + orderTotal + " kr. The pizzas will take " + Time + " minutes")
 
-// const wiki =
+
+// Stretch goals
+const wiki = "Hawaiian pizza (also known as Pizza Hawaii) is a pizza topped with tomato sauce, cheese, pineapple, and Canadian bacon or ham. Some versions may include peppers, mushrooms, or bacon.[citation needed] Pineapple as a pizza topping divides public opinion: Hawaiian was the most popular pizza in Australia in 1999, accounting for 15% of pizza sales,[1] and a 2015 review of independent UK takeaways operating through Just Eat found the Hawaiian pizza to be the most commonly available.[2] However, a 2016 survey of US adults had pineapple in the top three least favourite pizza toppings, behind anchovies and mushrooms.[3]"
+
+// Count the number of words in the string
+const WordCount = (str) => {
+  return str.split(" ").length
+}
+
 //
-// alert(wiki.length)
+const antalOrd=WordCount(wiki)
+alert("The number of words in the paragraph is: " + antalOrd)
+
+// Count the number pineapple in the string
+const WordMatch = (str) => {
+  return str.match(/pineapple/gi).length
+}
+
+const mentioned=WordMatch(wiki)
+alert("Pinapple is mentioned " + mentioned + " times in the text")
